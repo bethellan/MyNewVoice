b9d05b58ca6c84177dcb3ce5eb8ad88ac7b4b5c8
bethellan <bethellan@hotmail.com>
2025-10-11 22:33:05 +1300

    Add files via upload

---
+++ help.js
@@ -0,0 +1,14 @@
+window.HELP_PRESS = {
+  help: [
+    { icon: "🚪", text: "Please open the door." },
+    { icon: "🚪", text: "Please close the door." },
+    { icon: "🌀", text: "Please turn on the fan." },
+    { icon: "🌀", text: "Please turn off the fan." },
+    { icon: "🔥", text: "Please turn on the heater." },
+    { icon: "🔥", text: "Please turn off the heater." },
+    { icon: "🆘", text: "Please help me." },
+    { icon: "🙋", text: "I need assistance." },
+    { icon: "📣", text: "Can someone come here?" },
+    { icon: "🔘", text: "I pressed the call button." }
+  ]
+};
